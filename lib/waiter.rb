b62ddcbd3e24daacecf699ae21da220d446cd59f
Jenise Ruehle <jela07@gmail.com>
2020-03-23 12:28:34 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,16 @@
 class Waiter
+  attr_accessor :name, :years
+  
+  @@all = [] 
+  
+  def initialize(name, years)
+    @name = name 
+    @years = years
+    self.class.all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
 
 end
